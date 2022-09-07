8b5e56297dfd6089446e312ff0fd7d9233c24f3f
cmseibel <cam.m.seib@gmail.com>
2022-09-07 08:10:09 -0700

    Write first attempt at solution

---
+++ arrayGap.js
@@ -0,0 +1,36 @@
+/*
+    Largest gap in an array:
+    Given an unsorted array of length N, and we have to find the largest gap between any two elements
+    of the array. In simple words, find max(|Ai-Aj|) where 1 ≤ i ≤ N and 1 ≤ j ≤ N.
+
+    Examples:  
+
+    Input : arr = {3, 10, 6, 7}
+    Output : 7
+    Explanation :
+        Here, we can see largest gap can be
+        found between 3 and 10 which is 7
+
+    Input : arr = {-3, -1, 6, 7, 0}
+    Output : 10
+    Explanation :
+        Here, we can see largest gap can be 
+        found between -3 and 7 which is 10 
+*/
+function findLargestGap(array) {
+    let max = Number.MIN_VALUE;
+    let min = Number.MAX_VALUE;
+    array.forEach((value) => {
+        if(value > max) {
+            max = value;
+        }
+        if(value < min) {
+            min = value;
+        }
+    });
+    return max - min;
+}
+
+// Test cases
+console.log(findLargestGap([3, 10, 6, 7])) // expect 7
+console.log(findLargestGap([-3, -1, 6, 7, 0])); // expect 10
